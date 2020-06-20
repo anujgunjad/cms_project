72be0112c86d43992853b4154f9d0ec31f45a6f0
anujgunjad <a.gunjad11@gmail.com>
2020-06-20 20:14:51 +0530

    Initial Commit

---
+++ api/data/read_suspects.php
@@ -0,0 +1,61 @@
+<?php
+    //set headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Header: access");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Credentials: true");
+    header("Content-Type: application/json; charset= UTF-8");
+
+    //include Files
+    include_once '../../includes/config.php';
+    include_once '../objects/basic_details.php';
+
+    //initialize Database
+    $database = new Database();
+    $db = $database->getConnection();
+
+    //initialize Object
+    $data = new Basic($db);
+
+    $data->complaint_id = isset($_GET['complaint_id']) ?  $_GET['complaint_id'] : die();
+    //Query Product
+    $stmt = $data->read_suspect();
+
+    $num = $stmt->rowCount();
+
+    if($num > 0){
+        $data_arr = array();
+        $data_arr["suspects"] = array();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $data_items = array(
+                "complaint_number" => $complaint_number,
+                "suspect_id" => $suspect_id,
+                "suspect_name" => $suspect_name,
+                "suspect_address" => $suspect_address,
+                "email_id" => $email_id,
+                "domain_name" => $domain_name,
+                "upi_phone_no" => $upi_phone_no,
+                "upivpa" => $upivpa,
+                "software_name" => $software_name,
+                "complaint_action" => $complaint_action,
+                "pending_reason" => $pending_reason,
+                "remark" => $remark
+            );
+            array_push($data_arr["suspects"], $data_items);
+        }
+            // set response code - 200 OK
+            http_response_code(200);
+            // show products data in json format
+            echo json_encode($data_arr);
+    }
+    else {
+        // set response code - 404 Not found
+        http_response_code(404);   
+        // tell the user no products found
+        echo json_encode(
+            array("message" => "No suspects found.")
+        );
+    }
+?>
--- api/objects/basic_details.php
+++ api/objects/basic_details.php
@@ -1,8 +1,19 @@
 <?php
+ //include Files
+ include_once '../../includes/config.php';
+ //initialize Database
+ $database = new Database();
+ $db = $database->getConnection();
+ 
     class Basic{
+        //Database and table names
         private $conn;
         private $complainee_table = "basic_details";
         private $suspect_table = "suspect_details";
+        private $suspect_number_table = "suspect_numbers";
+        //Objects Properties
+        public $complaint_id;
+        public $suspect_id;
         // constructor with $db as database connection
         public function __construct($db){
             $this->conn = $db;
@@ -20,13 +31,16 @@
         
             return $stmt;
         }
-        function readAll_suspect(){
+        function read_suspect(){
 
-            //Select All Query
-            $query = "Select * from $this->suspect_table";
-
+            //Select Query
+            $query = "Select c.complaint_no as complaint_number, s.suspect_id, s.suspect_name, s.suspect_address, s.email_id, s.domain_name, s.upi_phone_no, s.upivpa, s.software_name, s.complaint_action, s.pending_reason, s.remark from $this->suspect_table s LEFT JOIN $this->complainee_table c ON s.complaint_id = c.complaint_id WHERE s.complaint_id = ?";
+            
             //Prepare query statement
             $stmt = $this->conn->prepare($query);
+           
+            // bind id of product to be updated
+           $stmt->bindParam(1, $this->complaint_id);
 
             //Execute query
             $stmt->execute();
@@ -34,4 +48,7 @@
             return $stmt;
         }
     }
+    // $read_suspect = new Basic($db);
+    // $stmt = $read_suspect->read_suspect();
+    // echo $stmt->rowCount();
 ?>
