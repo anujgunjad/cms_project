c9344591acfc0b13725602cf8367743937003a7c
anujgunjad <a.gunjad11@gmail.com>
2020-06-26 10:41:50 +0530

    Database Updated

---
+++ api/data/search.php
@@ -0,0 +1,114 @@
+<?php
+   //set headers
+   header("Access-Control-Allow-Origin: *");
+   header("Access-Control-Allow-Header: access");
+   header("Access-Control-Allow-Methods: GET");
+   header("Access-Control-Allow-Credentials: true");
+   header("Content-Type: application/json; charset= UTF-8");
+
+    // include database and object files
+    include_once '../../includes/config.php';
+    include_once '../objects/filter_objects.php';
+  
+    // get keywords
+    $keyword = isset($_GET["keyword"]) ? $_GET["keyword"] : die();
+    $category_id = isset($_GET["category_id"]) ? $_GET["category_id"] : die();
+    //database 
+    $database = new Database();
+    $db = $database->getConnection();
+
+    //initialize  Object
+    $data = new Filter($db);
+
+    //Applicant Search 
+    
+    if($category_id == 1 || $category_id == 2 || $category_id == 3){
+
+    $stmt = $data->searchApplicant($keyword, $category_id); 
+    $num = $stmt->rowCount();
+    if($num > 0){
+    $data_arr = array(); 
+    $data_arr["applicant"] = array();
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $data_items = array(
+            "complaint_id" => $complaint_id,
+            "complaint_no" => $complaint_no,
+            "complaint_date" => $complaint_date,
+            "app_name" => $ap_name,
+            "ap_age" => $ap_age,
+            "ap_mob" => $ap_mob,
+            "ap_address" => $ap_address,
+            "ap_country" => $ap_country,
+            "ap_state" => $ap_state,
+            "ap_city" => $ap_city,
+            "ap_pin_code" => $ap_pin_code,
+            "ap_adhar" => $ap_adhar,
+            "crime_type" => $crime_type,
+            "way_of_crime" => $way_of_crime,
+            "it_act" => $it_act,
+            "bh_dv" => $bh_dv,
+            "crime_date" => $crime_date,
+            "crime_time" => $crime_time,
+            "amount" => $amount,
+            "checker_name" => $checker_name,
+            "created_date" => $created_date,
+            "last_updated" => $last_updated
+        );
+        array_push($data_arr["applicant"], $data_items);
+    }
+      // set response code - 200 OK
+      http_response_code(200);
+      // show products data in json format
+      echo json_encode($data_arr);
+    }
+    
+    else{
+        // set response code - 404 Not found
+        http_response_code(404);
+        // tell the user no products found
+        echo json_encode(
+            array("message" => "No applicant found.")
+        );
+    }
+
+    }
+    
+    //Suspect Search
+    
+    else {
+        $stmt = $data->searchSuspect($keyword, $category_id);
+
+          
+        $num = $stmt->rowCount();
+        if($num > 0){
+        $data_arr = array(); 
+        $data_arr["applicant"] = array();
+    
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $data_items = array(
+                "complaint_id" => $complaint_id,
+                "complaint_no" => $complaint_no,
+                "app_name" => $ap_name
+            );
+            array_push($data_arr["applicant"], $data_items);
+        }
+          // set response code - 200 OK
+          http_response_code(200);
+          // show products data in json format
+          echo json_encode($data_arr);
+        }
+        
+        else{
+            // set response code - 404 Not found
+            http_response_code(404);
+            // tell the user no products found
+            echo json_encode(
+                array("message" => "No applicant found.")
+            );
+        }
+    }
+   
+?>
---
+++ api/objects/filter_objects.php
@@ -0,0 +1,87 @@
+<?php
+    //including Files
+    include_once '../../includes/config.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    class Filter{
+        //Database and table names
+        private $conn;
+        private $complainee_table = "basic_details";
+        private $suspect_table = "suspect_details";
+
+        //Suspect Number Info Tables
+        private $suspect_number_table = "suspect_numbers";
+        private $suspect_number_cdr_table = "suspect_number_cdr_info";
+        private $suspect_number_ipdr_table = "suspect_number_ipdr_info";
+        private $suspect_number_upi_table = "suspect_number_upi_info";
+
+            //Suspect Account Info Tables
+        private $suspect_account_table = "suspect_bank_accounts";
+        private $suspect_account_atm_table = "bank_accounts_atm";
+        private $suspect_account_iplogs_table = "bank_accounts_iplogs";
+        private $suspect_account_pan_table = "suspect_pan_info";
+
+            //Suspect E-wallet table
+        private $suspect_ewallet_table = "suspect_ewallet_info";
+
+            //Suspect Website table
+        private $suspect_website_table = "suspect_website_info";
+
+        // constructor with $db as database connection
+        public function __construct($db){
+            $this->conn = $db;
+        }
+        
+        function searchApplicant($keyword, $category_id){
+
+            if($category_id == 1){
+                 //query
+                 $query = "SELECT * FROM $this->complainee_table WHERE ap_name LIKE ?";
+            }
+            else if($category_id == 2){
+                //query
+                $query = "SELECT * FROM $this->complainee_table WHERE complaint_no LIKE ?";
+            }
+            else if($category_id == 3){
+                //query
+                $query = "SELECT * FROM $this->complainee_table WHERE ap_mob LIKE ?";
+            }
+          
+            $stmt = $this->conn->prepare($query);
+            
+            $keyword = "%$keyword%";
+
+            // bind
+            $stmt->bindParam(1, $keyword);
+           
+            // execute query
+            $stmt->execute();
+        
+            return $stmt;
+        }
+
+        function searchSuspect($keyword, $category_id){
+
+             if($category_id == 4){
+                 //query
+                 $query = "SELECT c.complaint_id, c.complaint_no, c.ap_name FROM $this->complainee_table c INNER JOIN $this->suspect_table s INNER JOIN $this->suspect_number_table n  INNER JOIN $this->suspect_account_table a WHERE s.suspect_name = ? OR n.suspect_name  = ? OR a.kyc_name = ?";
+             }
+
+             $stmt = $this->conn->prepare($query);
+            
+             $keyword = "%$keyword%";
+             
+             // bind
+             $stmt->bindParam(1, $keyword);
+             $stmt->bindParam(2, $keyword);
+             $stmt->bindParam(3, $keyword);
+
+             // execute query
+             $stmt->execute();
+        
+             return $stmt;
+         }
+    }
+?>
