fdb2e8b35eed6c52566e7567833298ef4f3b7ee7
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-06-30 13:51:13 +0530

    commit

--- Database/cms_project .sql
+++ Database/cms_project .sql
@@ -1,11 +1,4 @@
-﻿-- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
 
--- Host: 127.0.0.1
--- Generation Time: Jun 30, 2020 at 09:01 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
