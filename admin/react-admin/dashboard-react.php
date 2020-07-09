ab7492cbb35500f67899ac919f2144ffcc4b0225
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-07-09 15:57:01 +0530

    Commit

--- admin/react-admin/dashboard-react.php
+++ admin/react-admin/dashboard-react.php
@@ -34,13 +34,14 @@ var myChart = new Chart(ctx, {
 });
 // Bar Graph
 var ctx = document.getElementById("myChart");
+let subComplaintData = [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1, 3, 13, 7, 2, 16]
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["Online Bank Fraud", "Job Fraud", "OLX Fraud", "KYC Fraud", "Link Fraud", "2015-06", "2015-07", "2015-08", "2015-09", "2015-10", "2015-11", "2015-12"],
+    labels: ["Online Bank Fraud", "Job Fraud", "OLX Fraud", "KYC Fraud", "Link Fraud", "Screen Sharing App", "Fake Facebook", "Fake Instagram", "Facebook Hack", "Instagram Hack", "Gmail Hack", "Social Media Harassment","Whatsapp Harassment","Whatsapp Hack","Metrimonial Fraud","Custom Fraud","Other"],
     datasets: [{
-      label: '# of Tomatoes',
-      data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1],
+      label: 'Total Sub Complaint',
+      data: subComplaintData,
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -53,7 +54,12 @@ var myChart = new Chart(ctx, {
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
       ],
       borderColor: [
         'rgba(255,99,132,1)',
@@ -67,7 +73,12 @@ var myChart = new Chart(ctx, {
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
         'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
       ],
       borderWidth: 1
     }]
