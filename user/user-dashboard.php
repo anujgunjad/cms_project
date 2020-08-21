a54e6120154c95945d0ed5c73a6417f338e5a4d1
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-08-21 13:46:43 +0530

    COMMIT

--- user/user-dashboard.php
+++ user/user-dashboard.php
@@ -23,22 +23,8 @@ if (isset($_GET['logout'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!--Bootstrap-->
-    <link rel="stylesheet" type="text/css" href="../dependencies/bootstrap/dist/css/bootstrap.min.css">
-    <!--fontawesome-->
-    <link rel="stylesheet" href="../dependencies/fontawesome/css/all.css">
-    <!--External CSS-->
-    <link rel="stylesheet" href="style/userDashStyle.css" />
-    <title>CMS | User Dashboard</title>
-</head>
-<body>
 
+<?php include('user-header.php')?>
 
     <div class="main-dash container-fluid">
         <div class="side-bar">
@@ -102,6 +88,4 @@ if (isset($_GET['logout'])) {
             </div>
         </div>
     </div>
-</body>
-
-</html>
+<?php include('user-footer.php')?>
---
+++ user/user-header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Bootstrap-->
+    <link rel="stylesheet" type="text/css" href="../dependencies/bootstrap/dist/css/bootstrap.min.css">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="../dependencies/fontawesome/css/all.css">
+    <!--External CSS-->
+    <link rel="stylesheet" href="style/userDashStyle.css" />
+    <title>CMS | User Dashboard</title>
+</head>
+<body>
