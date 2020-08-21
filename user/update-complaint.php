fd243178f30bff368b440396095b9c1edb806d4b
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-08-21 16:30:09 +0530

    COMMIT

--- user/update-complaint.php
+++ user/update-complaint.php
@@ -1,3 +1,3 @@
 <?php include('user-header.php')?>
-    <h2>HELLO UPDATE</h2>
+    
 <?php include('user-footer.php')?>
--- user/user-dashboard.php
+++ user/user-dashboard.php
@@ -72,7 +72,7 @@ if (isset($_GET['logout'])) {
                             <td>".$row['ap_name']."</td>
                             <td>
                                 <div class='field'>
-                                    <a class='btn btn-success' href='update-complaint.php?".$row['complaint_id']."' name='number_form'>Edit</a>
+                                    <a style='color:#fff' class='btn btn-success' href='update-complaint.php?".$row['complaint_id']."' name='number_form'>Edit</a>
                                     <button type='button' class='btn btn-danger'>Delete</button>
                                 </div>
                             </td>
