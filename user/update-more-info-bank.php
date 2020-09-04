b32868292ccea2d3f0628668b2b696e279287691
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-09-04 10:07:05 +0530

    COMMIT

--- user/update-more-info-bank.php
+++ user/update-more-info-bank.php
@@ -70,6 +70,7 @@ const timeDateFormatter = (arry) => {
                 .then(res => res.json())
                 .then((data) => {
                     this.setState({pans: data.pan});
+                    console.log(data.pan)
                 })
                 .catch(console.log)
         }
@@ -82,21 +83,27 @@ const timeDateFormatter = (arry) => {
                 <div class="ui segment blue mt-4 mb-5">
                 <h1 class=" h1 h2-complaint">Permanent account number [PAN]</h1>                
                         {
-                            this.state.pans ? this.state.pans.map((pan) => (    
+                            this.state.pans ? this.state.pans.map((pan, i) => (    
                             <table class="ui celled table">   
                                 <tbody>
                                 <tr>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Permanent account number</h4>{pan.pan?pan.pan:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">PAN Verified</h4>{pan.pan_verified?pan.pan_verified:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">PAN Username</h4>{pan.pan_username?pan.pan_username:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Aadhaar Number</h4>{pan.adhar_number?pan.adhar_number:"अभी तक दर्ज नहीं है"}</td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1"><span style={{color:"red"}}>[{i+1}]</span> Permanent account number</h4><input class="rounded py-2 mt-1 px-2" id={"pan_" + pan.pan_info_id } type="text" placeholder={pan.pan} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">PAN Verified</h4><input class="rounded py-2 mt-1 px-2" id={"pan_verified_" + pan.pan_info_id } type="text" placeholder={pan.pan_verified} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">PAN Username</h4><input class="rounded py-2 mt-1 px-2" id={"pan_username_" + pan.pan_info_id } type="text" placeholder={pan.pan_username} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Aadhaar Number</h4><input class="rounded py-2 mt-1 px-2" id={"pan_adhar_number_" + pan.pan_info_id } type="text" placeholder={pan.adhar_number} /></td>
                                 </tr>
                                 <tr>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Income Tax</h4>{pan.income_tax?pan.adhar_number:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">GST</h4>{pan.gst_in?pan.gst_in:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">TIN</h4>{pan.tin?pan.tin:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Sales Tax</h4>{pan.sales_tax?pan.sales_tax:"अभी तक दर्ज नहीं है"}</td>
-                                </tr>                   
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Income Tax</h4><input class="rounded py-2 mt-1 px-2" id={"pan_income_tax_" + pan.pan_info_id } type="text" placeholder={pan.income_tax} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">GST</h4><input class="rounded py-2 mt-1 px-2" id={"pan_gst_in_" + pan.pan_info_id } type="text" placeholder={pan.gst_in} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">TIN</h4><input class="rounded py-2 mt-1 px-2" id={"pan_tin_" + pan.pan_info_id } type="text" placeholder={pan.tin} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Sales Tax</h4><input class="rounded py-2 mt-1 px-2" id={"pan_sales_tax_" + pan.pan_info_id } type="text" placeholder={pan.sales_tax} /></td>
+                                </tr>  
+                                <tr>
+                                <td><button class="ui button update-button mt-2 py-3 px-5" onClick={() => this.updatePanInfo(pan.pan_info_id)}>Update</button></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                </tr>                 
                            </tbody>
                         </table>
                         ))
@@ -144,11 +151,11 @@ const timeDateFormatter = (arry) => {
                 <h1 class=" h1 h2-complaint">ATM Information</h1>                
                              
                         {
-                            this.state.atms ? this.state.atms.map((atm) => (    
+                            this.state.atms ? this.state.atms.map((atm, i) => (    
                             <table class="ui celled table">   
                                 <tbody>
                                 <tr>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Complaint Number</h4>{atm.complaint_number?atm.complaint_number:"अभी तक दर्ज नहीं है"}</td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1"> <span style={{color:"red"}}>[{i+1}]</span> Complaint Number</h4>{atm.complaint_number?atm.complaint_number:"अभी तक दर्ज नहीं है"}</td>
                                     <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">ATM Footage</h4>{atm.atm_footage?atm.atm_footage:"अभी तक दर्ज नहीं है"}</td>
                                     <td class={dateFormatter(atm.email_sent)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(atm.email_received)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल भेजने की तारीख</h4>{dateFormatter(atm.email_sent)!="00-00-0000"?dateFormatter(atm.email_sent):"मेल नहीं भेजा गया"}</td>
                                     <td class={dateFormatter(atm.email_received)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(atm.email_received)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल प्राप्त करने की तारीख</h4>{dateFormatter(atm.email_received)!="00-00-0000"?dateFormatter(atm.email_received):"मेल अभी तक नहीं मिला "}</td>
@@ -200,11 +207,11 @@ const timeDateFormatter = (arry) => {
                 <h1 class=" h1 h2-complaint">IP Logs</h1>                
                              
                 {
-                            this.state.iplogs ? this.state.iplogs.map((iplog) => (    
+                            this.state.iplogs ? this.state.iplogs.map((iplog, i) => (    
                             <table class="ui celled table">   
                                 <tbody>
                                 <tr>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Complaint Number</h4>{iplog.complaint_number?iplog.complaint_number:"अभी तक दर्ज नहीं है"}</td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1"><span style={{color:"red"}}>[{i+1}]</span> Complaint Number</h4>{iplog.complaint_number?iplog.complaint_number:"अभी तक दर्ज नहीं है"}</td>
                                     <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">iplog </h4>{iplog.iplog?iplog.iplog:"अभी तक दर्ज नहीं है"}</td>
                                     <td class={dateFormatter(iplog.email_sent)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(iplog.email_received)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल भेजने की तारीख</h4>{dateFormatter(iplog.email_sent)!="00-00-0000"?dateFormatter(iplog.email_sent):"मेल नहीं भेजा गया"}</td>
                                     <td class={dateFormatter(iplog.email_received)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(iplog.email_received)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल प्राप्त करने की तारीख</h4>{dateFormatter(iplog.email_received)!="00-00-0000"?dateFormatter(iplog.email_received):"मेल अभी तक नहीं मिला "}</td>
