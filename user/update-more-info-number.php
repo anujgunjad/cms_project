d18551e754b998e9ff9205fc48724d6d32f4d539
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-09-04 20:03:54 +0530

    COMMIT

--- user/update-more-info-number.php
+++ user/update-more-info-number.php
@@ -214,10 +214,70 @@ const currentDate = (date) => {
                 .then(res => res.json())
                 .then((data) => {
                     this.setState({ipdr: data.ipdr});
-                    // console.log(this.state.ipdr);
+                    console.log(this.state.ipdr);
                 })
         }
-        
+        updateCdrInfo = (cdr_id) => {
+            let ids = idsFetcher(),
+                    numId = ids[0],
+                    comId = ids[1];
+            const cdrArry = this.state.cdr.filter(cdr => cdr.cdr_id == cdr_id);
+            const cdr = cdrArry[0];
+            let complaint_id_cdr = comId,
+                number_id_cdr = numId,
+                cdr_id_cdr = cdr_id,
+                cdr_cdr = document.getElementById("cdr_cdr_" + cdr.cdr_id ).value?document.getElementById("cdr_cdr_" + cdr.cdr_id).value : cdr.cdr,
+                email_sent_cdr = document.getElementById("cdr_email_sent_" + cdr.cdr_id ).value?document.getElementById("cdr_email_sent_" + cdr.cdr_id).value : cdr.email_sent,
+                email_received_cdr = document.getElementById("cdr_email_received_" + cdr.cdr_id ).value?document.getElementById("cdr_email_received_" + cdr.cdr_id).value : cdr.email_received,
+                imei_cdr = document.getElementById("cdr_imei_" + cdr.cdr_id ).value?document.getElementById("cdr_imei_" + cdr.cdr_id).value : cdr.imei,
+                imsi_cdr = document.getElementById("cdr_imsi_" + cdr.cdr_id ).value?document.getElementById("cdr_imsi_" + cdr.cdr_id).value : cdr.imsi,
+                location_cdr = document.getElementById("cdr_location_" + cdr.cdr_id ).value?document.getElementById("cdr_location_" + cdr.cdr_id).value : cdr.location,
+                location_date_cdr = document.getElementById("cdr_location_date_" + cdr.cdr_id ).value?document.getElementById("cdr_location_date_" + cdr.cdr_id).value : cdr.location_date,
+                location_time_cdr = document.getElementById("cdr_location_time_" + cdr.cdr_id ).value?document.getElementById("cdr_location_time_" + cdr.cdr_id).value : cdr.location_date,
+                night_loc_cdr = document.getElementById("cdr_night_loc_" + cdr.cdr_id ).value?document.getElementById("cdr_night_loc_" + cdr.cdr_id).value : cdr.night_loc,
+                service_name_cdr = document.getElementById("cdr_service_name_" + cdr.cdr_id ).value?document.getElementById("cdr_service_name_" + cdr.cdr_id).value : cdr.service_name,
+                suspect_number_cdr = document.getElementById("cdr_suspect_number_" + cdr.cdr_id ).value?document.getElementById("cdr_suspect_number_" + cdr.cdr_id).value : cdr.suspect_number,
+                cdr_pdf_cdr = cdr.cdr_pdf,
+                created_date_cdr = cdr.created_date,
+                cdr_update_date = new Date(),
+                last_updated_cdr = currentDate(cdr_update_date);
+            fetch("../api/data/update_number_cdr.php", { 
+                // Adding method type 
+                method: "POST", 
+                // Adding body or contents to send 
+                body: JSON.stringify({ 
+                    complaint_id_cdr ,
+                    number_id_cdr,
+                    cdr_id_cdr,
+                    cdr_cdr,
+                    email_sent_cdr,
+                    email_received_cdr,
+                    imei_cdr,
+                    imsi_cdr,
+                    location_cdr,
+                    location_date_cdr,
+                    location_time_cdr,
+                    night_loc_cdr,
+                    service_name_cdr,
+                    suspect_number_cdr,
+                    cdr_pdf_cdr,
+                    created_date_cdr,
+                    last_updated_cdr,
+                })
+            })
+            
+                // update done
+                .then(
+                        swal({
+                            title: 'Updated Successfuly',
+                            icon: 'success',
+                            button: 'Next',
+                        })
+                        .then(() => {
+                            location.reload();
+                        })
+                    ); 
+        }        
         render(){
             return(
                 <div>
@@ -230,11 +290,15 @@ const currentDate = (date) => {
                             <table class="ui celled table">   
                                 <tbody>
                                 <tr>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1"><span style={{color:"red"}}>[{i+1}]</span> IP Adress</h4>{ipdr.ipdr?ipdr.ipdr:"अभी तक दर्ज नहीं है"}</td>
-                                    <td class={dateFormatter(ipdr.email_sent)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(ipdr.email_sent)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल भेजने की तारीख</h4>{dateFormatter(ipdr.email_sent)!="00-00-0000"?dateFormatter(ipdr.email_sent):"मेल नहीं भेजा गया"}</td>
-                                    <td class={dateFormatter(ipdr.email_received)!="00-00-0000"?"success-text":"danger-text"} style={{fontSize:"1.11rem"}}><h4 class={dateFormatter(ipdr.email_received)!="00-00-0000"?"ui header mb-1 mt-1 success-text":"ui header mb-1 mt-1 danger-text"}>ईमेल प्राप्त करने की तारीख</h4>{dateFormatter(ipdr.email_received)!="00-00-0000"?dateFormatter(ipdr.email_received):"मेल अभी तक नहीं मिला "}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Location</h4>{ipdr.location?ipdr.location:"अभी तक दर्ज नहीं है"}</td>
-                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">वेबसाइट</h4>{ipdr.website?ipdr.website:"अभी तक दर्ज नहीं है"}</td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1"><span style={{color:"red"}}>[{i+1}]</span> IP Adress</h4><input class="rounded py-2 mt-1 px-2" id={"ipdr_ipdr_" + ipdr.ipdr_id} type="text" placeholder={ipdr.ipdr} /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">ईमेल भेजने की तारीख</h4><input class="rounded py-2 mt-1 pl-2 pr-5" id={"ipdr_email_sent_" + ipdr.ipdr_id } type="date" /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">ईमेल प्राप्त करने की तारीख</h4><input class="rounded py-2 mt-1 pl-2 pr-5" id={"ipdr_email_received_" + ipdr.ipdr_id } type="date" /></td>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">Location</h4><input class="rounded py-2 mt-1 px-2" id={"ipdr_location_" + ipdr.ipdr_id} type="text" placeholder={ipdr.location} /></td>
+                                </tr>
+                                <tr>
+                                    <td style={{fontSize:"1.11rem"}}><h4 class="ui header theme-color mb-1 mt-1">वेबसाइट</h4><input class="rounded py-2 mt-1 px-2" id={"ipdr_website_" + ipdr.ipdr_id} type="text" placeholder={ipdr.website} /></td>
+                                    <td><button class="ui button update-button mt-2 py-3 px-5" onClick={() => this.updateIpInfo(ipdr.ipdr_id)}>Update</button></td>
+                                
                                 </tr>
                                
                                     
